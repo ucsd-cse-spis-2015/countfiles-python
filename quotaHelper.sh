143d0c5c7fe7324e24eac015c024ece7ee8de297
Phill Conrad <pconrad@ucsd.edu>
2015-08-25 09:27:01 -0700

    add $HOME

--- quotaHelper.sh
+++ quotaHelper.sh
@@ -2,13 +2,13 @@
 
 # Three commands that help me fix quota problems
 echo "Here is some help with your quota problem, " $USER
-quota -vs
-du -sh *
-du -sh .[A-z]*
+quota -vs 
+du -sh $HOME/*
+du -sh $HOME/.[A-z]* 
 
 echo "Here are the ones with M in them"
 
-du -sh * | grep "M"
-du -sh .[A-z]* | grep "M"
+du -sh $HOME/* | grep "M"
+du -sh $HOME/.[A-z]* | grep "M"
 
 
